cc7e23c110aec712da59b20aa79c522d64df2245
KB <kb.choi@jax.org>
2020-09-10 01:31:32 -0400

    [kbchoi] Adapting for scATAC-seq

--- scfastq.c
+++ scfastq.c
@@ -16,14 +16,16 @@ KSEQ_INIT(gzFile, gzread)
 KHASH_SET_INIT_STR(str)
 
 void print_usage(char **argv) {
-    fprintf(stderr, "Usage: %s [-den] [-c chunk_size] [-o output.fastq] i1.fastq.gz r1.fastq.gz r2.fastq.gz\n", argv[0]);
-    fprintf(stderr, " -or-  %s [-den] [-c chunk_size] [-o output.fastq] r2.fastq.gz\n", argv[0]);
+    fprintf(stderr, "Usage: %s [-den] [-b barcode_size] [-u umi_size] [-c chunk_size] [-o output.fastq] i1.fastq.gz r1.fastq.gz r2.fastq.gz r3.fastq.gz\n", argv[0]);
+    fprintf(stderr, " -or-  %s [-den] [-b barcode_size] [-u umi_size] [-c chunk_size] [-o output.fastq] r1.fastq.gz r3.fastq.gz\n", argv[0]);
 }
 
 
 int main(int argc, char **argv) {
     bool debugMode = false;
     bool dedup = true;
+    int umiSize = 0;
+    int barcodeSize = 16;
     int chunkSize = 0;
     char *filenameOutputOption = NULL;
     char *experimentIDOption = NULL;
@@ -31,8 +33,11 @@ int main(int argc, char **argv) {
 
     opterr = 0;
 
-    while ((option = getopt(argc, argv, "c:de:no:")) != -1) {
+    while ((option = getopt(argc, argv, "b:c:de:no:u:")) != -1) {
         switch (option) {
+            case 'b':
+                barcodeSize = atoi(optarg);
+                break;
             case 'c':
                 chunkSize = atoi(optarg);
                 break;
@@ -47,6 +52,9 @@ int main(int argc, char **argv) {
                 break;
             case 'o':
                 asprintf(&filenameOutputOption, "%s", optarg);
+                break;
+            case 'u':
+                umiSize = atoi(optarg);
                 break;
             default:
                 print_usage(argv);
@@ -66,20 +74,26 @@ int main(int argc, char **argv) {
     /* reads */
     char* filenameR2 = NULL;
 
+    /* reads */
+    char* filenameR3 = NULL;
+
     /* output file */
-    char* filenameOutputFASTQ = NULL;
+    char* filenameOutputFASTQ1 = NULL;
+    char* filenameOutputFASTQ2 = NULL;
 
     /* three file input */
-    int threeFiles = 1;
+    int fourFiles = 1;
 
-    if ((argc - optind) == 3) {
-        threeFiles = 1;
+    if ((argc - optind) == 4) {
+        fourFiles = 1;
         filenameI1 = argv[optind];
         filenameR1 = argv[optind+1];
         filenameR2 = argv[optind+2];
-    } else if ((argc - optind) == 1) {
-        threeFiles = 0;
-        filenameR2 = argv[optind];
+        filenameR3 = argv[optind+3];
+    } else if ((argc - optind) == 2) {
+        fourFiles = 0;
+        filenameR1 = argv[optind];
+        filenameR3 = argv[optind+1];
     } else {
         print_usage(argv);
         exit(EXIT_FAILURE);
@@ -92,12 +106,14 @@ int main(int argc, char **argv) {
     }
 
     if (debugMode) {
-        if (threeFiles) {
+        if (fourFiles) {
             fprintf(stderr, "I1: %s\n", filenameI1);
-            fprintf(stderr, "R1: %s\n", filenameR1);
         }
-
-        fprintf(stderr, "R2: %s\n", filenameR2);
+        fprintf(stderr, "R1: %s\n", filenameR1);
+        if (fourFiles) {
+            fprintf(stderr, "R2: %s\n", filenameR2);
+        }
+        fprintf(stderr, "R3: %s\n", filenameR3);
 
         if (filenameOutputOption) {
             fprintf(stderr, "Output: %s\n", filenameOutputOption);
@@ -125,12 +141,12 @@ int main(int argc, char **argv) {
     kseq_t *seqI1;
     FILE *fpI1;
 
-    /* setup R1 */
-    gzFile gzR1;
-    kseq_t *seqR1;
-    FILE *fpR1;
+    /* setup R2 */
+    gzFile gzR2;
+    kseq_t *seqR2;
+    FILE *fpR2;
 
-    if (threeFiles) {
+    if (fourFiles) {
         fpI1 = fopen(filenameI1, "rb");
 
         if (fpI1 == NULL) {
@@ -141,48 +157,65 @@ int main(int argc, char **argv) {
         gzI1 = gzdopen(fileno(fpI1), "r");
         seqI1 = kseq_init(gzI1);
 
-        fpR1 = fopen(filenameR1, "rb");
+        fpR2 = fopen(filenameR2, "rb");
 
-        if (fpR1 == NULL) {
-            fprintf(stderr, "Can't open: %s\n", filenameR1);
+        if (fpR2 == NULL) {
+            fprintf(stderr, "Can't open: %s\n", filenameR2);
             exit(EXIT_FAILURE);
         }
 
-        gzR1 = gzdopen(fileno(fpR1), "r");
-        seqR1 = kseq_init(gzR1);
+        gzR2 = gzdopen(fileno(fpR2), "r");
+        seqR2 = kseq_init(gzR2);
     } else {
         gzI1 = NULL;
         seqI1 = NULL;
         fpI1 = NULL;
 
-        gzR1 = NULL;
-        seqR1 = NULL;
-        fpR1 = NULL;
+        gzR2 = NULL;
+        seqR2 = NULL;
+        fpR2 = NULL;
     }
 
 
-    /* setup R2 */
-    gzFile gzR2;
-    kseq_t *seqR2;
-    FILE *fpR2 = fopen(filenameR2, "rb");
+    /* setup R1 */
+    gzFile gzR1;
+    kseq_t *seqR1;
+    FILE *fpR1 = fopen(filenameR1, "rb");
 
-    if (fpR2 == NULL) {
-        fprintf(stderr, "Can't open: %s\n", filenameR2);
+    if (fpR1 == NULL) {
+        fprintf(stderr, "Can't open: %s\n", filenameR1);
         exit(EXIT_FAILURE);
     }
 
-    gzR2 = gzdopen(fileno(fpR2), "r");
-    seqR2 = kseq_init(gzR2);
+    gzR1 = gzdopen(fileno(fpR1), "r");
+    seqR1 = kseq_init(gzR1);
 
-    FILE *fpOut = stdout;
+    /* setup R3 */
+    gzFile gzR3;
+    kseq_t *seqR3;
+    FILE *fpR3 = fopen(filenameR3, "rb");
+
+    if (fpR3 == NULL) {
+        fprintf(stderr, "Can't open: %s\n", filenameR3);
+        exit(EXIT_FAILURE);
+    }
+
+    gzR3 = gzdopen(fileno(fpR3), "r");
+    seqR3 = kseq_init(gzR3);
+
+    FILE *fpOut1 = stdout;
+    FILE *fpOut2 = stdout;
 
     if (filenameOutputOption) {
         if (chunkSize > 0) {
-            asprintf(&filenameOutputFASTQ, "%s_0.fastq", filenameOutputOption);
+            asprintf(&filenameOutputFASTQ1, "%s_0.R1.fastq", filenameOutputOption);
+            asprintf(&filenameOutputFASTQ2, "%s_0.R3.fastq", filenameOutputOption);
         } else {
-            asprintf(&filenameOutputFASTQ, "%s", filenameOutputOption);
+            asprintf(&filenameOutputFASTQ1, "%s.R1.fastq", filenameOutputOption);
+            asprintf(&filenameOutputFASTQ2, "%s.R3.fastq", filenameOutputOption);
         }
-        fpOut = fopen(filenameOutputFASTQ, "w");
+        fpOut1 = fopen(filenameOutputFASTQ1, "w");
+        fpOut2 = fopen(filenameOutputFASTQ2, "w");
     }
 
     if (experimentIDOption) {
@@ -206,37 +239,30 @@ int main(int argc, char **argv) {
     // allocate a hash table
     khash_t(str) *h = kh_init(str);
 
-    /*
-    CR = R1.sequence[:16]
-    CY = R1.quality[:16]
-    UR = R1.sequence[16:]
-    UY = R1.quality[16:]
-    */
-
-    char cr[17];
-    char cy[17];
-    char ur[11];
-    char uy[11];
+    char cr[barcodeSize + 1];
+    char cy[barcodeSize + 1];
+    char ur[umiSize + 1];
+    char uy[umiSize + 1];
     char *bc = NULL;   // I1.seq
     char *qt = NULL;   // I1.qual
-    memset(cr, '\0', 17);
-    memset(cy, '\0', 17);
-    memset(ur, '\0', 11);
-    memset(uy, '\0', 11);
+    memset(cr, '\0', barcodeSize + 1);
+    memset(cy, '\0', barcodeSize + 1);
+    memset(ur, '\0', umiSize + 1);
+    memset(uy, '\0', umiSize + 1);
 
     //name, comment, seq, qual;
     int nCounter = 0;
     int nFiles = 0;
 
-    while (kseq_read(seqR2) >= 0) {
-        if (threeFiles) {
-            kseq_read(seqR1);
+    while (kseq_read(seqR1) >= 0) {
+        if (fourFiles) {
             kseq_read(seqI1);
+            kseq_read(seqR2);
 
             if (dedup) {
-                int buf_size = seqR1->seq.l;
+                int buf_size = seqR2->seq.l;
                 char *buf = malloc(buf_size);
-                snprintf(buf, buf_size, "%s", seqR1->seq.s);
+                snprintf(buf, buf_size, "%s", seqR2->seq.s);
 
                 int absent;
                 k = kh_put(str, h, buf, &absent);
@@ -248,22 +274,27 @@ int main(int argc, char **argv) {
                 }
             }
 
-            memcpy(cr, &seqR1->seq.s[0], 16);
-            memcpy(cy, &seqR1->qual.s[0], 16);
-            memcpy(ur, &seqR1->seq.s[16], 10);
-            memcpy(uy, &seqR1->qual.s[16], 10);
+            memcpy(cr, &seqR2->seq.s[0], barcodeSize);
+            memcpy(cy, &seqR2->qual.s[0], barcodeSize);
+            memcpy(ur, &seqR2->seq.s[barcodeSize], umiSize);
+            memcpy(uy, &seqR2->qual.s[barcodeSize], umiSize);
 
             bc = seqI1->seq.s;
             qt = seqI1->qual.s;
 
-            fprintf(fpOut, "@%s|||CR|||%s|||CY|||%s|||UR|||%s|||UY|||%s|||BC|||%s|||QT|||%s|||CID|||%s-%s %s\n",
-                    seqR2->name.s, cr, cy, ur, uy, bc, qt, cr, experimentID, seqR2->comment.s);
+            fprintf(fpOut1, "@%s|||CR|||%s|||CY|||%s|||UR|||%s|||UY|||%s|||BC|||%s|||QT|||%s|||CID|||%s-%s %s\n",
+                    seqR1->name.s, cr, cy, ur, uy, bc, qt, cr, experimentID, seqR1->comment.s);
+            fprintf(fpOut2, "@%s|||CR|||%s|||CY|||%s|||UR|||%s|||UY|||%s|||BC|||%s|||QT|||%s|||CID|||%s-%s %s\n",
+                    seqR3->name.s, cr, cy, ur, uy, bc, qt, cr, experimentID, seqR3->comment.s);
         } else {
-            fprintf(fpOut, "@%s|||CR||||||CY||||||UR||||||UY||||||BC||||||QT||||||CID|||%s %s\n",
-                    seqR2->name.s, experimentID, seqR2->comment.s);
+            fprintf(fpOut1, "@%s|||CR||||||CY||||||UR||||||UY||||||BC||||||QT||||||CID|||%s %s\n",
+                    seqR1->name.s, experimentID, seqR1->comment.s);
+            fprintf(fpOut2, "@%s|||CR||||||CY||||||UR||||||UY||||||BC||||||QT||||||CID|||%s %s\n",
+                    seqR3->name.s, experimentID, seqR3->comment.s);
         }
 
-        fprintf(fpOut, "%s\n+\n%s\n", seqR2->seq.s, seqR2->qual.s);
+        fprintf(fpOut1, "%s\n+\n%s\n", seqR1->seq.s, seqR1->qual.s);
+        fprintf(fpOut2, "%s\n+\n%s\n", seqR3->seq.s, seqR3->qual.s);
 
         nCounter = nCounter + 1;
 
@@ -280,41 +311,52 @@ int main(int argc, char **argv) {
         }
 
         if ((chunkSize > 0) && (nCounter % chunkSize) == 0) {
-            fclose(fpOut);
+            fclose(fpOut1);
+            fclose(fpOut2);
             nFiles = nFiles + 1;
 
             if (debugMode) {
-                fprintf(stderr, "Generated: %s\n", filenameOutputFASTQ);
+                fprintf(stderr, "Generated: %s\n", filenameOutputFASTQ1);
+                fprintf(stderr, "Generated: %s\n", filenameOutputFASTQ2);
             }
 
-            asprintf(&filenameOutputFASTQ, "%s_%d.fastq", filenameOutputOption, nFiles);
+            asprintf(&filenameOutputFASTQ1, "%s_%d.R1.fastq", filenameOutputOption, nFiles);
+            asprintf(&filenameOutputFASTQ2, "%s_%d.R3.fastq", filenameOutputOption, nFiles);
 
             if (debugMode) {
-                fprintf(stderr, "Generating: %s\n", filenameOutputFASTQ);
+                fprintf(stderr, "Generating: %s\n", filenameOutputFASTQ1);
+                fprintf(stderr, "Generating: %s\n", filenameOutputFASTQ2);
             }
 
-            fpOut = fopen(filenameOutputFASTQ, "w");
+            fpOut1 = fopen(filenameOutputFASTQ1, "w");
+            fpOut2 = fopen(filenameOutputFASTQ2, "w");
         }
     }
 
-    if (threeFiles) {
+    if (fourFiles) {
         kseq_destroy(seqI1);
         fclose(fpI1);
         gzclose(gzI1);
 
-        kseq_destroy(seqR1);
-        fclose(fpR1);
-        gzclose(gzR1);
+        kseq_destroy(seqR2);
+        fclose(fpR2);
+        gzclose(gzR2);
     }
 
-    kseq_destroy(seqR2);
-    fclose(fpR2);
-    gzclose(gzR2);
+    kseq_destroy(seqR1);
+    fclose(fpR1);
+    gzclose(gzR1);
 
-    fclose(fpOut);
+    kseq_destroy(seqR3);
+    fclose(fpR3);
+    gzclose(gzR3);
+
+    fclose(fpOut1);
+    fclose(fpOut2);
 
     if (debugMode) {
-        fprintf(stderr, "Generated: %s\n", filenameOutputFASTQ);
+        fprintf(stderr, "Generated: %s\n", filenameOutputFASTQ1);
+        fprintf(stderr, "Generated: %s\n", filenameOutputFASTQ2);
 
         timeTemp = clock() - timeStart;
         double time_total = ((double)timeTemp)/CLOCKS_PER_SEC; // in seconds
